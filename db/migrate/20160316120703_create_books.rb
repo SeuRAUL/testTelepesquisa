30abc1da0464c34fb3e6a173a40ac80df896b881
jorge medeiros <jorge@telepesquisa.com.br>
2016-03-16 10:29:12 -0300

    CRUD Books

--- db/migrate/20160316120703_create_books.rb
+++ db/migrate/20160316120703_create_books.rb
@@ -3,6 +3,8 @@ class CreateBooks < ActiveRecord::Migration
     create_table :books do |t|
     	t.string 	:name
     	t.float 	:price
+    	t.integer 	:quantity
+    	
     	t.references :stock
 
       t.timestamps
