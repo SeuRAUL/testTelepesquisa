8371283d24a15ef9b93dedaf41204963f48dfff8
jorge medeiros <jorge@telepesquisa.com.br>
2016-03-16 09:22:47 -0300

    create model stock

--- db/migrate/20160316120703_create_books.rb
+++ db/migrate/20160316120703_create_books.rb
@@ -1,8 +1,9 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-    	t.string :name
-    	t.float :price
+    	t.string 	:name
+    	t.float 	:price
+    	t.references :stock
 
       t.timestamps
     end
